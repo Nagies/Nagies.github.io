c88388083a9759758a58f40b419c9aa6dff4abd7
Jesse Nagelberg <jnagelberg@gmail.com>
2015-05-11 16:09:16 -0700

    commiting untracked changes

---
+++ assets/js/scroll-trigger.js
@@ -0,0 +1,8 @@
+// $( window ).scroll( function() {
+//     var scrollPosition = window.pageYOffset
+//     var skillsViewTop = Math.floor($('#s').position().top)
+//     if ( scrollPosition > skillsViewTop && scrollPosition < skillsViewTop + 10) {
+//         console.log('hello')
+//         $( window).off( 'scroll' )
+//     }
+// });
