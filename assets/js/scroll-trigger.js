668e81269bdbbde420070b0d809d4d5c68688bc1
Jesse Nagelberg <jnagelberg@gmail.com>
2015-07-06 15:13:53 -0400

    new website!

--- assets/js/scroll-trigger.js
+++
@@ -1,8 +0,0 @@
-// $( window ).scroll( function() {
-//     var scrollPosition = window.pageYOffset
-//     var skillsViewTop = Math.floor($('#s').position().top)
-//     if ( scrollPosition > skillsViewTop && scrollPosition < skillsViewTop + 10) {
-//         console.log('hello')
-//         $( window).off( 'scroll' )
-//     }
-// });
--- assets/js/smooth-scroll.js
+++
@@ -1,7 +0,0 @@
-
-$('a[href*="#"]').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 400);
-    return false;
-});
