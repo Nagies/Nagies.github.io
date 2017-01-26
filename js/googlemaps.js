7e672a18a6070b7896946fbd19cb3baa5a09b94e
Jesse Nagelberg <jnagelberg@gmail.com>
2017-01-25 19:00:42 -0800

    commented out js map

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -1,70 +1,70 @@
 
-window.marker = null;
+// window.marker = null;
 
-function initialize() {
-	var map;
+// function initialize() {
+// 	var map;
 
-	var mymap = new google.maps.LatLng(40.7590615, -73.969231);
+// 	var mymap = new google.maps.LatLng(40.7590615, -73.969231);
 
-	var style = [ 
-		{ "featureType": "road", 
-		   "elementType": 
-		   "labels.icon", 
-		   "stylers": [ 
-			  { "saturation": -100 }, 
-			  { "gamma": 1 }, 
-			  { "visibility": "off" }, 
-			  { "hue": "#e6ff00" } 
-		   ] 
-		},
-		{ "elementType": "geometry", "stylers": [ 
-			{ "saturation": -100 } 
-		  ] 
-		},
-		{ "elementType": "labels.text", "stylers": [ { "saturation": -100 } ] }
-	];
+// 	var style = [ 
+// 		{ "featureType": "road", 
+// 		   "elementType": 
+// 		   "labels.icon", 
+// 		   "stylers": [ 
+// 			  { "saturation": -100 }, 
+// 			  { "gamma": 1 }, 
+// 			  { "visibility": "off" }, 
+// 			  { "hue": "#e6ff00" } 
+// 		   ] 
+// 		},
+// 		{ "elementType": "geometry", "stylers": [ 
+// 			{ "saturation": -100 } 
+// 		  ] 
+// 		},
+// 		{ "elementType": "labels.text", "stylers": [ { "saturation": -100 } ] }
+// 	];
 
-	var mapOptions = {
-		// SET THE CENTER
-		center: mymap,
+// 	var mapOptions = {
+// 		// SET THE CENTER
+// 		center: mymap,
 
-		// SET THE MAP STYLE & ZOOM LEVEL
-		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		zoom:11,
-		scrollwheel: false,
+// 		// SET THE MAP STYLE & ZOOM LEVEL
+// 		mapTypeId: google.maps.MapTypeId.ROADMAP,
+// 		zoom:11,
+// 		scrollwheel: false,
 
-		// SET THE BACKGROUND COLOUR
-		backgroundColor:"#eeeeee",
+// 		// SET THE BACKGROUND COLOUR
+// 		backgroundColor:"#eeeeee",
 
-		// REMOVE ALL THE CONTROLS EXCEPT ZOOM
-		panControl:false,
-		zoomControl:true,
-		mapTypeControl:false,
-		scaleControl:false,
-		streetViewControl:false,
-		overviewMapControl:false,
-		zoomControlOptions: {
-			style:google.maps.ZoomControlStyle.SMALL
-		}
+// 		// REMOVE ALL THE CONTROLS EXCEPT ZOOM
+// 		panControl:false,
+// 		zoomControl:true,
+// 		mapTypeControl:false,
+// 		scaleControl:false,
+// 		streetViewControl:false,
+// 		overviewMapControl:false,
+// 		zoomControlOptions: {
+// 			style:google.maps.ZoomControlStyle.SMALL
+// 		}
 
-	}
-	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+// 	}
+// 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-	// SET THE MAP TYPE
-	var mapType = new google.maps.StyledMapType(style, {name:"Grayscale"});    
-	map.mapTypes.set('grey', mapType);
-	map.setMapTypeId('grey');
+// 	// SET THE MAP TYPE
+// 	var mapType = new google.maps.StyledMapType(style, {name:"Grayscale"});    
+// 	map.mapTypes.set('grey', mapType);
+// 	map.setMapTypeId('grey');
 
-	//CREATE A CUSTOM PIN ICON
-	var marker_image = 'images/pin.png';
-	var pinIcon = new google.maps.MarkerImage(marker_image,null,null, null,new google.maps.Size(21, 34));    
+// 	//CREATE A CUSTOM PIN ICON
+// 	var marker_image = 'images/pin.png';
+// 	var pinIcon = new google.maps.MarkerImage(marker_image,null,null, null,new google.maps.Size(21, 34));    
 
-	marker = new google.maps.Marker({
-	  position: mymap,
-	  map: map,
-	  icon: pinIcon,
-	  title: 'Our Location'
-	});
-}
+// 	marker = new google.maps.Marker({
+// 	  position: mymap,
+// 	  map: map,
+// 	  icon: pinIcon,
+// 	  title: 'Our Location'
+// 	});
+// }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+// google.maps.event.addDomListener(window, 'load', initialize);
